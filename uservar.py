5a9e854eca07b79234074d78eccdec6a28a60934
ハックモンカー <46002790+hackmonker@users.noreply.github.com>
2023-09-12 01:50:29 +0530

    Update uservar.py

--- uservar.py
+++ uservar.py
@@ -99,9 +99,9 @@ AUTOINSTALL = 'Yes'
 REPOID = 'repository.lokiwizard'
 # How often you would like it to check for build updates in days'
 # Url to Addons.xml file in your repo folder(this is so we can get the latest version)
-REPOADDONXML = 'https://'
+REPOADDONXML = 'https://raw.githubusercontent.com/hackmonker/repository.lokiwizard/master/repo/zips/addons.xml'
 # Url to folder zip is located in
-REPOZIPURL = 'https://'
+REPOZIPURL = 'https://github.com/hackmonker/repository.lokiwizard/tree/master/repo/zips/repository.lokiwizard'
 #########################################################
 
 #########################################################
